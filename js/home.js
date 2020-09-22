ed9423bca191db06de119e187044a775d44962c4
ratchaya166 <ratchaya166@gmail.com>
2020-09-22 14:36:20 +0700

    add photo slide

--- js/home.js
+++ js/home.js
@@ -1,4 +1,31 @@
+
 document.addEventListener('prechange', function(event) {
     document.querySelector('ons-toolbar .center')
       .innerHTML = event.tabItem.getAttribute('label');
+  });
+
+
+  
+  new Vue({
+    el: '#app',
+    template: '#main',
+    data() {
+      return {
+        carouselIndex: 0,
+        items: {
+          BLUE: '#085078',
+          DARK: '#373B44',
+          ORANGE: '#D38312'
+        },
+        dots: {
+          textAlign: 'center',
+          fontSize: '30px',
+          color: '#fff',
+          position: 'absolute',
+          bottom: '40px',
+          left: 0,
+          right: 0
+        }
+      };
+    }
   });
